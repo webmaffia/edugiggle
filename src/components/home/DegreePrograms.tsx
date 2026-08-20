4c08f62c7db8442f2d69f18f9ac874a5191bd6c9
Javed <javed.mistari@webmaffia.com>
2026-08-20 17:40:01 +0530

    next js

--- src/components/home/DegreePrograms.tsx
+++ src/components/home/DegreePrograms.tsx
@@ -83,7 +83,7 @@ const MASTERS_COURSES: Course[] = [
 ];
 
 const CHECK_ICON = (
-  <svg className="h-3.5 w-3.5 text-accent shrink-0" fill="currentColor" viewBox="0 0 20 20">
+  <svg className="h-3.5 w-3.5 text-primary shrink-0" fill="currentColor" viewBox="0 0 20 20">
     <path clipRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" fillRule="evenodd"></path>
   </svg>
 );
